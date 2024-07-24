3cffa2a463dfdef54e8d02a46e1b8c0458bb5255
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-24 13:44:50 +0530

    ..

---
+++ src/main/java/org/example/armstrong.java
@@ -0,0 +1,32 @@
+package org.example;
+import java.util.Scanner;
+
+public class armstrong {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int num= in.nextInt();
+
+//        for( int i=0;i<100;i++){
+////       if(isarmstrong(num)){
+//           System.out.println( i +" ");
+//       }
+//    }
+//
+//    static void isarmstrong() {
+//        int original = num;
+//        int sum=0;
+//        while(num>0){
+//            int rem= num %  10;
+//            num = num / 10;
+//            sum =  sum + rem*rem*rem;
+//        }
+//        if (sum==original){
+//            System.out.println("Entered number is armstrong");
+//        }else {
+//            System.out.println("Entered number is not armstrong");
+//        }
+//    }
+    }}
+
+
+
