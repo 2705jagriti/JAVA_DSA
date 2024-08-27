7b1c7cd4e38d3c6da0609efdad3b00e0c9f0aed0
2705jagriti <jagritichaudhary842@gmail.com>
2024-08-28 00:09:03 +0530

    commit all

--- src/main/java/Revision/ex_2.java
+++ src/main/java/Revision/ex_2.java
@@ -6,19 +6,24 @@ import java.util.Scanner;
 public class ex_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println( shorted( new int[] {1,2,3,4,5,5}));
+        System.out.println( shorted( new int[] {1,2,6,4,5,5}));
 
     }
-    static boolean shorted (int[] arr){
+    static int  shorted (int[] arr){
+        int temp;
             for (int i = 0; i < arr.length ; i++) {
-                for (int j = i; j < arr.length ; j++) {
-                    int a=arr[0];
-                    int b = arr[1];
-                    if(a<b){
-                        return true ;
-                    }
+                for (int j = i+1; j < arr.length ; j++) {
+                   if(arr[i]<arr[j]){
+                       temp = arr[i];
+                       arr[i]= arr[j];
+                       arr[j]=temp;
+                   }
+
                 }
+                int ans;
+                System.out.println(arr[i]);
             }
-            return false;
+
+return ans ;
     }
 }
