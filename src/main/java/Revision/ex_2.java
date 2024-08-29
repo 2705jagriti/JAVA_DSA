5fa654c67a64ac9efc2ee42924dadcab72bbdd87
2705jagriti <jagritichaudhary842@gmail.com>
2024-08-29 23:56:08 +0530

    commit all

--- src/main/java/Revision/ex_2.java
+++ src/main/java/Revision/ex_2.java
@@ -9,21 +9,22 @@ public class ex_2 {
         System.out.println( shorted( new int[] {1,2,6,4,5,5}));
 
     }
-    static int  shorted (int[] arr){
+    static int  shorted (int[] arr) {
         int temp;
-            for (int i = 0; i < arr.length ; i++) {
-                for (int j = i+1; j < arr.length ; j++) {
-                   if(arr[i]<arr[j]){
-                       temp = arr[i];
-                       arr[i]= arr[j];
-                       arr[j]=temp;
-                   }
-
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-                int ans;
-                System.out.println(arr[i]);
-            }
 
-return ans ;
+            }
+            int ans;
+            System.out.println(arr[i]);
+        }
+
+        return arr[i];
     }
 }
--- src/main/java/Searching/seach_in_range.java
+++ src/main/java/Searching/seach_in_range.java
@@ -2,6 +2,24 @@ package Searching;
 
 public class seach_in_range {
     public static void main(String[] args) {
+searchinrangr(int[] new{1,2,3,4,4}
+    }
+    static  int searchinrangr(int[] arr , int target ,int start ,int end ){
+        if(arr.length==0){
+            return -1;
+        }
 
+        for (int index = start; index <=end; index++) {
+          int element = arr[index];
+            if(element==target){
+                return index;
+            }
+
+        }
+        // this line will execute if non of the retune is possible
+        // target not found
+
+
+        return -1;
     }
 }
